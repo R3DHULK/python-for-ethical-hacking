3cce0580c7fdf5c6972ca09229ee1d342c85d12d
Sumalya Chatterjee <105884682+R3DHULK@users.noreply.github.com>
2023-01-03 20:51:38 +0530

    Dns Enumeration Tool in python

---
+++ dns-enumeration.py
@@ -0,0 +1,23 @@
+import dns.resolver
+
+# Set the target domain and record type
+print ('''\033[93m]
+    **********************************************
+    *       Simple DNS Enumeration Tool          *
+    *  github page : https://github.com/r3dhulk  *
+    **********************************************
+       ''')
+target_domain = input("\033[92m [*] Hulk Wants The URL: ")
+record_types = ["A", "AAAA", "CNAME", "MX", "NS", "SOA", "TXT"]
+# Create a DNS resolver
+resolver = dns.resolver.Resolver()
+for record_type in record_types:
+    # Perform DNS lookup for the specified domain and record type
+    try:
+        answers = resolver.resolve(target_domain, record_type)
+    except dns.resolver.NoAnswer:
+        continue
+    # Print the answers
+    print(f"{record_type} records for {target_domain}:")
+    for rdata in answers:
+        print(f" {rdata}")
